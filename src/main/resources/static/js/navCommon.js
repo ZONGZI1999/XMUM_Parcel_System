22f02398f0d32a9ec314e1e0a27cb588d3b78e80
Zongqi Zheng <798012131@qq.com>
2020-12-10 00:59:37 +0800

    Fix some bugs and replace jQuery Source

---
+++ src/main/resources/static/js/navCommon.js
@@ -0,0 +1,9 @@
+$().ready(function(){
+    //导航栏
+    $('#myParcel').click(function () {
+        window.location.href = 'myParcel';
+    });
+    $('#parcelQuery').click(function (){
+        window.location.href = 'parcelQuery';
+    })
+})
